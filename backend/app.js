239381811ee7e42d1d770c520fbe73d47746e47e
Vinesh Kumar <Vineshk711@gmail.com>
2021-07-20 14:48:54 +0000

    tried to connect to DB

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,29 @@
+require('dotenv').config()
+
 const express = require('express')
+const mongoose = require('mongoose')
+
+const PORT = process.env.PORT
+const databaseURL = process.env.DB_CONNECTION
 
 const app = express()
+
+// connectng to DB
+
+// mongoose.connect(databaseURL, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true
+// })
+// .then(() => {
+//     console.log('DB CONNECTED')
+// })
+
+ console.log(process.env.abc)
+
+// connection the server
+
+app.listen(PORT, () => {
+    console.log(`app is listening on port ${PORT}`)
+})
 
