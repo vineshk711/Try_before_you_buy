7487ff5c73bade1398eafa1a792304f27627cea5
Vinesh Kumar <Vineshk711@gmail.com>
2021-07-23 18:39:58 +0000

    adding signup controller

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,7 +1,20 @@
+const User = require('../models/user')
+
+
 exports.signup = ( req, res) => {
-    res.json({
-        message: "Sign up route works"
-    })
+    const user = new User(req.body);
+    user.save((err, user) => {
+        if (err) {
+            return res.status(400).json({
+                error: "Unable to add user into DB"
+            });
+        }
+        res.json({
+            name: user.name,
+            email: user.email,
+            id: user._id
+        });
+    });
 }
 
 exports.signout = ( req, res ) => {
