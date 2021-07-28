95c46ee1bb3b0a52bb475815e008855ba35ddb0e
Vinesh Kumar <Vineshk711@gmail.com>
2021-07-28 12:58:33 +0000

    completed category route

--- backend/controllers/category.js
+++ backend/controllers/category.js
@@ -8,7 +8,7 @@ exports.getCategoryById = (req, res, next, id) => {
                 error: err
             })
         }
-        res.category = category
+        req.category = category
         next()
     })
 }
@@ -28,4 +28,48 @@ exports.createCategory = (req, res) => {
 
 exports.getCategory = (req, res) => {
     return res.json(req.category)
+}
+
+exports.getAllCategories = (req, res) => {
+    Category.find().exec( (err, categories) => {
+        if(err){
+            return res.status(400).json({
+                message: "Unable to find any category",
+                error: err
+            })
+        }
+        res.json({categories})
+    })
+}
+
+exports.updateCategory = (req, res) => {
+    const category = req.category
+    category.name = req.body.name
+    
+    category.save( (err, updatedCategory) => {
+        if(err) {
+            return req.status(400).json({
+                message: "Unable to update category",
+                error: err
+            })
+        }
+        res.json({
+            message: `Updated category to ${updatedCategory.name}`
+        })
+    })
+}
+
+exports.removeCategory = (req, res) => {
+    const category = req.category
+    category.remove( (err, removedCategory) => {
+        if(err || !removedCategory) {
+            return res.status(400).json({
+                message: "Unable to delete category",
+                error: err
+            })
+        }
+        res.json({
+            message: `${removedCategory.name} is successfully deleted`
+        })
+    })
 }
--- backend/routes/category.js
+++ backend/routes/category.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const { getUserById } = require('../controllers/user')
-const { getCategoryById, createCategory, getCategory } = require('../controllers/category')
+const { getCategoryById, createCategory, getCategory, getAllCategories, updateCategory, removeCategory } = require('../controllers/category')
 const { isSignedIn, isAuthenticated, isAdmin } = require('../controllers/auth')
 
 // params
@@ -10,10 +10,19 @@ router.param("userId", getUserById)
 router.param("categoryId", getCategoryById)
 
 
-// routes
-router.post("/category/create/:userId", isSignedIn, isAuthenticated, createCategory)
-router.get("/category/:categoryId", getCategory)
+// ROUTES
+// create
+router.post("/category/create/:userId", isSignedIn, isAuthenticated, isAdmin, createCategory)
 
+// read
+router.get("/category/:categoryId", getCategory)
+router.get("/categories", getAllCategories)
+
+// update
+router.put("/category/:categoryId/:userId", isSignedIn, isAuthenticated, isAdmin, updateCategory)
+
+// delete
+router.delete("/category/:categoryId/:userId", isSignedIn, isAuthenticated, isAdmin, removeCategory)
 
 
 module.exports = router
